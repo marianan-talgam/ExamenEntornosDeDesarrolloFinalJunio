144c00b18739389a84e824da8303fb06ee5504cc
Paco <dummy@paco.com>
2026-06-16 01:38:17 +0200

    Pruebas

---
+++ src/ejercicio/Pruebas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package ejercicio;
+
+/**
+ *
+ * @author Pacoep
+ */
+import java.util.Scanner;
+
+public class Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declaración de variables
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Aquí se prueba");
+    }
+
+}
